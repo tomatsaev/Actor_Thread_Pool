a1c5dc93ebf025764e87b990e7fa66dbf9e6dd33
tomatsaev <50755334+tomatsaev@users.noreply.github.com>
2022-10-31 12:04:26 +0200

    Update Promise.java

--- src/main/java/bgu/atd/a1/Promise.java
+++ src/main/java/bgu/atd/a1/Promise.java
@@ -7,13 +7,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
  * this class represents a deferred result i.e., an object that eventually will
  * be resolved to hold a result of some operation, the class allows for getting
  * the result once it is available and registering a callback that will be
- * called once the result is available.
- *
- * Note for implementors: you may add methods and synchronize any of the
- * existing methods in this class *BUT* you must be able to explain why the
- * synchronization is needed. In addition, the methods you add can only be
- * private, protected or package protected - in other words, no new public
- * methods
+ * called once the result is available. 
  *
  * @param <T>
  *            the result type, <boolean> resolved - initialized ;
@@ -99,11 +93,6 @@ public class Promise<T>{
 	 * add a callback to be called when this object is resolved. If while
 	 * calling this method the object is already resolved - the callback should
 	 * be called immediately
-	 *
-	 * Note that in any case, the given callback should never get called more
-	 * than once, in addition, in order to avoid memory leaks - once the
-	 * callback got called, this object should not hold its reference any
-	 * longer.
 	 *
 	 * @param callback
 	 *            the callback to be called when the promise object is resolved
