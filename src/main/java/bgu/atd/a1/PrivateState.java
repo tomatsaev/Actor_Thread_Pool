292e19efa4b6b9045908ea11a20f44f4ac924768
tomatsaev <50755334+tomatsaev@users.noreply.github.com>
2022-10-31 12:00:09 +0200

    Update PrivateState.java

--- src/main/java/bgu/atd/a1/PrivateState.java
+++ src/main/java/bgu/atd/a1/PrivateState.java
@@ -7,7 +7,6 @@ import java.util.List;
 /**
  * an abstract class that represents private states of an actor
  * it holds actions that the actor has executed so far 
- * IMPORTANT: You can not add any field to this class.
  */
 public abstract class PrivateState implements Serializable {
 	
